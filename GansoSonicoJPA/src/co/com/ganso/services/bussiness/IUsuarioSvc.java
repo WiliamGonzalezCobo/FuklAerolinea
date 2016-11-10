59d25fcbf1a493f8e9605015140bda5417a62f75
WilliamGonzalez <WilliamGonzalez@DESKTOP-7NDPJ1J>
2016-11-09 19:54:52 -0500

    Se corrige local y se agrega entity en el jpa

--- GansoSonicoJPA/src/co/com/ganso/services/bussiness/IUsuarioSvc.java
+++ GansoSonicoJPA/src/co/com/ganso/services/bussiness/IUsuarioSvc.java
@@ -1,7 +1,10 @@
 package co.com.ganso.services.bussiness;
 
+import javax.ejb.Local;
+
 import co.com.ganso.entities.Usuario;
 
+@Local
 public interface IUsuarioSvc {
 	
 	public boolean acceder(Usuario usuario) throws Exception;
